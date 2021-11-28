43e1cb32f215fd309421241103e2a2ef3daf0129
Bartosz <bartek2@onet.eu>
2021-11-28 18:42:52 +0100

    Add wesbos es lint config

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,12 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-test("renders learn react link", () => {
+test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test("filing test", () => {
+test('filing test', () => {
   expect(2 + 2).toBe(4);
 });
