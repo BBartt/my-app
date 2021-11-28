37781869fd72ca82cf5d68c29912323cd4797075
Bartosz <bartek2@onet.eu>
2021-11-28 19:25:40 +0100

    asdsafhjer

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,12 @@ function App(): JSX.Element {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Learn React
         </a>
       </header>
